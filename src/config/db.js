42455c7a16d31e091302645cea43fe36fdc9fc86
ZisanSarker <sakibuzzamanjisan@gmail.com>
2024-10-17 06:59:49 +0600

    db config update

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,44 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
 
-const connectDB = async () =>{
+const connectDB = async () => {
+    if (!process.env.mongoURL) {
+        console.error('MongoDB connection string is missing!'.bgRed.white);
+        process.exit(1);
+    }
+
     try {
-        await mongoose.connect(process.env.mongoURL);
+        await mongoose.connect(process.env.mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`Connected to Database: ${mongoose.connection.host}`.bgCyan.white);
     } catch (error) {
-        console.log(`Error in connecting to Database: ${error}`.bgRed.white)
+        console.log(`Error in connecting to Database: ${error.message}`.bgRed.white);
+        process.exit(1); // Exit application with error code 1 in case of failure to connect to database.
     }
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected'.bgGreen.white);
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.error(`Database connection error: ${err.message}`.bgRed.white);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database connection disconnected'.bgYellow.white);
+    });
+
+    mongoose.connection.on('reconnected', () => {
+        console.log('Database reconnected'.bgGreen.white);
+    });
+
+    process.on('SIGINT', async () => {
+        await mongoose.connection.close();
+        console.log('Database connection closed due to application termination'.bgYellow.white);
+        process.exit(0);
+    });
 };
 
-module.exports  = connectDB;
+module.exports = connectDB;
